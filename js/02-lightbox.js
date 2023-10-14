f32637bcef93dccb879d02283d018ab05028eea6
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-15 00:17:19 +0300

    Update 02-lightbox.js
    add 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+let markup = '';
+galleryItems.forEach(({preview, original, description}) =>  
+markup += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`)
+;
+
+const galleryList = document.querySelector('.gallery')
+galleryList.innerHTML = markup;
+
+galleryList.style.display = 'flex';
+galleryList.style.alighnItems = 'center';
+galleryList.style.justifyContent = 'center';
+galleryList.style.flexWrap = 'wrap';
+galleryList.style.rowGap = '10px';
+galleryList.style.columnGap = '10px';
+galleryList.style.marginBottom = '50px';
+
+const galleryImages = document.querySelectorAll('img');
+galleryImages.forEach(image => {image.style.width = '300px';  image.style.heigth = '200px'});
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {overlayOpacity: 0.98, captionsData: 'alt'});
+
+lightbox.on('show.simplelightbox', function () {
+  lightbox.refresh()
+});
