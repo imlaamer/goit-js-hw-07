5e34afde04bd62b8dacfec64f808b06bbd7284b4
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-15 00:16:50 +0300

    Update 01-gallery.js
    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+let markup = '';
+galleryItems.forEach(({preview, original, description}) =>  
+markup += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`)
+;
+
+const galleryList = document.querySelector('.gallery');
+galleryList.innerHTML = markup;
+
+
+galleryList.style.display = 'flex';
+galleryList.style.alighnItems = 'center';
+galleryList.style.justifyContent = 'center';
+galleryList.style.flexWrap = 'wrap';
+galleryList.style.rowGap = '10px';
+galleryList.style.columnGap = '10px';
+galleryList.style.marginBottom = '50px';
+
+const galleryImages = document.querySelectorAll('img');
+galleryImages.forEach(image => {image.style.width = '300px';  image.style.heigth = '200px'});
+
+galleryList.addEventListener('click', openModal);
+
+function openModal(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') return; 
+
+let url  = event.target.dataset.source;
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src='${url}' width="800" height="600">
+        </img>
+    </div>
+ `,
+ {
+onShow: (instance) => {
+    if (event.target.nodeName !== 'IMG') {
+        return; 
+    }
+instance.element('.modal').addEventListener('click',  closeModal);
+window.addEventListener('keydown', closeModal) 
+},
+onClose: () => {
+ window.removeEventListener('keydown',closeModal)
+}});
+
+instance.show();
+
+function closeModal({code, target}) {
+    if(code === 'Escape' || target.nodeName === 'IMG')
+    instance.close()
+}
+}
