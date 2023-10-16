260137fbc5f9d278da349fb2509bff158215f5b1
imlaamer <lolitayenik@ukr.net>
2023-10-16 17:37:09 +0300

    +formatting, -style, +attr width & heigth

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,52 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
+let markup = "";
+galleryItems.forEach(
+  ({ preview, original, description }) =>
+    (markup += `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" width="300" heigth="200"/>
+        </a>
+      </li>`)
+);
 
-let markup = '';
-galleryItems.forEach(({preview, original, description}) =>  
-markup += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`)
-;
-
-const galleryList = document.querySelector('.gallery');
+const galleryList = document.querySelector(".gallery");
 galleryList.innerHTML = markup;
 
-
-galleryList.style.display = 'flex';
-galleryList.style.alighnItems = 'center';
-galleryList.style.justifyContent = 'center';
-galleryList.style.flexWrap = 'wrap';
-galleryList.style.rowGap = '10px';
-galleryList.style.columnGap = '10px';
-galleryList.style.marginBottom = '50px';
-
-const galleryImages = document.querySelectorAll('img');
-galleryImages.forEach(image => {image.style.width = '300px';  image.style.heigth = '200px'});
-
-
-galleryList.addEventListener('click', openModal);
+galleryList.addEventListener("click", openModal);
 
 function openModal(event) {
-    event.preventDefault()
-    if (event.target.nodeName !== 'IMG') return; 
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
 
-let url  = event.target.dataset.source;
+  let url = event.target.dataset.source;
 
-const instance = basicLightbox.create(`<div class="modal"><img src='${url}' width="800" height="600"></img></div>`,
- {
-onShow: (instance) => {
-instance.element('.modal').addEventListener('click',  closeModal);
-window.addEventListener('keydown', closeModal) 
-},
-onClose: () => {
- window.removeEventListener('keydown',closeModal)
-}});
+  const instance = basicLightbox.create(
+    `<div class="modal"><img src='${url}' width="800" height="600"></img></div>`,
+    {
+      onShow: (instance) => {
+        instance.element(".modal").addEventListener("click", closeModal);
+        window.addEventListener("keydown", closeModal);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
 
-instance.show();
+  instance.show();
 
-function closeModal({code, target}) {
-    if(code === 'Escape' || target.nodeName === 'IMG')
-    instance.close()
+  function closeModal({ code, target }) {
+    if (code === "Escape" || target.nodeName === "IMG") instance.close();
+  }
 }
-}
-
-
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-let markup = '';
-galleryItems.forEach(({preview, original, description}) =>  
-markup += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`)
-;
+let markup = "";
+galleryItems.forEach(
+  ({ preview, original, description }) =>
+    (markup += `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" width="300" heigth="200"/>
+        </a>
+      </li>`)
+);
 
-const galleryList = document.querySelector('.gallery')
+const galleryList = document.querySelector(".gallery");
 galleryList.innerHTML = markup;
 
-galleryList.style.display = 'flex';
-galleryList.style.alighnItems = 'center';
-galleryList.style.justifyContent = 'center';
-galleryList.style.flexWrap = 'wrap';
-galleryList.style.rowGap = '10px';
-galleryList.style.columnGap = '10px';
-galleryList.style.marginBottom = '50px';
-
-const galleryImages = document.querySelectorAll('img');
-galleryImages.forEach(image => {image.style.width = '300px';  image.style.heigth = '200px'});
-
-
-
-const lightbox = new SimpleLightbox('.gallery a', {overlayOpacity: 0.98, captionsData: 'alt' , captionDelay: 250});
-
-lightbox.on('show.simplelightbox', function () {
-  lightbox.refresh()
+const lightbox = new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.98,
+  captionsData: "alt",
+  captionDelay: 250,
 });
 
+lightbox.on("show.simplelightbox", function () {
+  lightbox.refresh();
+});
